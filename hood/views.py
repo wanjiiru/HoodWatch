873dc941e20376abe8735b6785a9d6468b18efb9
wanjiiru <liz240@live.com>
2018-08-03 12:52:24 +0300

    add first view , home view

--- hood/views.py
+++ hood/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url = '/accounts/login')
 def home(request):
-    pass
+    return render(request,'home.html',locals())
+
