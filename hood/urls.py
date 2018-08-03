0af24a550486cc5f390d93686a0c903a31c33e5e
wanjiiru <liz240@live.com>
2018-08-03 17:38:32 +0300

    add url to display businesses

--- hood/urls.py
+++ hood/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
         url(r'^$',views.home,name='home'),
         url(r'^business/view',views.display_business,name = 'viewbiz'),
         url(r'^business/',views.create_business,name = 'business'),
-         url(r'^accounts/', include('registration.backends.simple.urls')),
-         url(r'^profile/create/',views.create_profile,name = 'profile'),
+        url(r'^accounts/', include('registration.backends.simple.urls')),
+        url(r'^profile/create/',views.create_profile,name = 'profile'),
 
 ]
