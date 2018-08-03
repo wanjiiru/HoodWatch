e9a339455efe3e9e64a2eace34aaa70d6ce92a37
wanjiiru <liz240@live.com>
2018-08-03 15:35:05 +0300

    add Neighbourhood class

--- hood/models.py
+++ hood/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.db import IntegrityError
+
+
 
 # Create your models here.
+class Neighbourhood(models.Model):
+    name = models.CharField(max_lenghth = 65)
+    location = models.CharField(max_length = 65)
+    occupants = models.PositiveIntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+    def save_hood(self):
+        self.save()
+
+    def delete_hood(self):
+        self.delete()
+
+
+
+        
