8e715ef06335aebdd39733da0115b395ea8220a7
wanjiiru <liz240@live.com>
2018-08-03 15:59:57 +0300

    register models in the admin

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Business, Neighbourhood,Profile,Post
+
+
 
 # Register your models here.
+admin.site.register(Business)
+admin.site.register(Neighbourhood)
+admin.site.register(Profile)
+admin.site.register(Post)
+
--- hood/models.py
+++ hood/models.py
@@ -21,6 +21,7 @@ class Neighbourhood(models.Model):
 
     def delete_hood(self):
         self.delete()
+        
 class Profile(models.Model):
     name = models.CharField(max_length = 65, blank=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -53,14 +54,14 @@ class Business(models.Model):
         self.delete()
 
 
-    class Post(models.Model):
-        user = models.ForeignKey(Profile, on_delete=models.CASCADE)
-        post = models.TextField(max_length=300)
-        hood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
-        location = models.CharField(max_length = 65)
+class Post(models.Model):
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    post = models.TextField(max_length=300)
+    hood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    location = models.CharField(max_length = 65)
         
-        def __str__(self):
-            return self.post
+    def __str__(self):
+        return self.post
 
 
 
