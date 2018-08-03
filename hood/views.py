1cc7ec3a8f7a19e9257e758a10bb7a7bfec7393e
wanjiiru <liz240@live.com>
2018-08-03 16:13:00 +0300

    add business view

--- hood/views.py
+++ hood/views.py
@@ -1,8 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from .models import Post,Profile,Neighbourhood,Business
 
 # Create your views here.
 @login_required(login_url = '/accounts/login')
 def home(request):
+    posts = Post.objects.all()
+    businesses = Business.objects.all()
+    hoods = Neighbourhood.objects.all()
     return render(request,'home.html',locals())
 
+def create_business(request):
+    current_user = request.user
+    owner = Profile.get_by_id(current_user)
+    hood = Neighbourhood.objects.all()
+    
