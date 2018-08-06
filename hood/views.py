166d59f8d3c0f8334714b79a560adbddc6f45d06
wanjiiru <liz240@live.com>
2018-08-06 21:28:47 +0300

    add create hood template

--- hood/views.py
+++ hood/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from .models import Post,Profile,Neighbourhood,Business,Join
 from django.contrib import messages
-from . forms import ProfileForm,BusinessForm,PostForm
+from . forms import ProfileForm,BusinessForm,PostForm,CreateHoodForm
 from django.contrib.auth.models import User
 
 # Create your views here.
